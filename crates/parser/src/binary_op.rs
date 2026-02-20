624ee8a9c33b392987ac1e124db729b10cd7e2ae
Prestgg1 <prestgg12@gmail.com>
2026-02-21 00:07:42 +0400

    Fixed Binary Op Algorithm

--- crates/parser/src/binary_op.rs
+++ crates/parser/src/binary_op.rs
@@ -7,176 +7,53 @@ use tokenizer::tokens::Token;
 
 pub fn parse_expression<'a>(tokens: &mut Tokens) -> Result<Expr<'a>, ParserError> {
     let expr = parse_single_expr(tokens)?;
-    match tokens.peek() {
-        Some(SpannedToken {
-            token: Token::Add, ..
-        })
-        | Some(SpannedToken {
-            token: Token::Subtract,
-            ..
-        })
-        | Some(SpannedToken {
-            token: Token::Multiply,
-            ..
-        })
-        | Some(SpannedToken {
-            token: Token::Divide,
-            ..
-        })
-        | Some(SpannedToken {
-            token: Token::Modulo,
-            ..
-        })
-        | Some(SpannedToken {
-            token: Token::Greater,
-            ..
-        })
-        | Some(SpannedToken {
-            token: Token::Less, ..
-        })
-        | Some(SpannedToken {
-            token: Token::Equal,
-            ..
-        })
-        | Some(SpannedToken {
-            token: Token::NotEqual,
-            ..
-        })
-        | Some(SpannedToken {
-            token: Token::Not, ..
-        })
-        | Some(SpannedToken {
-            token: Token::And, ..
-        })
-        | Some(SpannedToken {
-            token: Token::Or, ..
-        })
-        | Some(SpannedToken {
-            token: Token::GreaterEqual,
-            ..
-        })
-        | Some(SpannedToken {
-            token: Token::LessEqual,
-            ..
-        }) => {}
-        _ => return Ok(expr),
-    }
-
-    parse_binary_op_with_precedence(&expr, tokens, 0)
+    parse_binary_op_with_precedence(expr, tokens, 0)
 }
 
 fn parse_binary_op_with_precedence<'a>(
-    left: &Expr<'a>,
+    mut left: Expr<'a>,
     tokens: &mut Tokens,
     min_precedence: u8,
 ) -> Result<Expr<'a>, ParserError> {
-    let mut result = Expr::Void;
     loop {
         let op = match tokens.peek() {
-            Some(SpannedToken {
-                token: Token::Add,
-                span,
-            }) => Operation::Add,
-            Some(SpannedToken {
-                token: Token::Subtract,
-                span,
-                ..
-            }) => Operation::Subtract,
-
-            Some(SpannedToken {
-                token: Token::Multiply,
-                span,
-                ..
-            }) => Operation::Multiply,
-
-            Some(SpannedToken {
-                token: Token::Divide,
-                span,
-                ..
-            }) => Operation::Divide,
-
-            Some(SpannedToken {
-                token: Token::Modulo,
-                span,
-                ..
-            }) => Operation::Modulo,
-
-            Some(SpannedToken {
-                token: Token::Greater,
-                span,
-                ..
-            }) => Operation::Greater,
-
-            Some(SpannedToken {
-                token: Token::Less,
-                span,
-                ..
-            }) => Operation::Less,
-
-            Some(SpannedToken {
-                token: Token::Equal,
-                span,
-                ..
-            }) => Operation::Equal,
-
-            Some(SpannedToken {
-                token: Token::NotEqual,
-                span,
-                ..
-            }) => Operation::NotEqual,
-
-            Some(SpannedToken {
-                token: Token::Not,
-                span,
-                ..
-            }) => Operation::Not,
-
-            Some(SpannedToken {
-                token: Token::And,
-                span,
-                ..
-            }) => Operation::And,
-
-            Some(SpannedToken {
-                token: Token::Or,
-                span,
-                ..
-            }) => Operation::Or,
-
-            Some(SpannedToken {
-                token: Token::GreaterEqual,
-                span,
-                ..
-            }) => Operation::GreaterEqual,
-
-            Some(SpannedToken {
-                token: Token::LessEqual,
-                span,
-                ..
-            }) => Operation::LessEqual,
-
-            None | Some(_) => {
-                break;
-            }
+            Some(SpannedToken { token: Token::Add, .. })         => Operation::Add,
+            Some(SpannedToken { token: Token::Subtract, .. })    => Operation::Subtract,
+            Some(SpannedToken { token: Token::Multiply, .. })    => Operation::Multiply,
+            Some(SpannedToken { token: Token::Divide, .. })      => Operation::Divide,
+            Some(SpannedToken { token: Token::Modulo, .. })      => Operation::Modulo,
+            Some(SpannedToken { token: Token::Greater, .. })     => Operation::Greater,
+            Some(SpannedToken { token: Token::Less, .. })        => Operation::Less,
+            Some(SpannedToken { token: Token::Equal, .. })       => Operation::Equal,
+            Some(SpannedToken { token: Token::NotEqual, .. })    => Operation::NotEqual,
+            Some(SpannedToken { token: Token::And, .. })         => Operation::And,
+            Some(SpannedToken { token: Token::Or, .. })          => Operation::Or,
+            Some(SpannedToken { token: Token::GreaterEqual, .. })=> Operation::GreaterEqual,
+            Some(SpannedToken { token: Token::LessEqual, .. })   => Operation::LessEqual,
+            _ => break,
         };
+
         let precedence = operator_precedence(&op);
         if precedence < min_precedence {
             break;
         }
+
         tokens.next();
-        /*BUG: This line has to be fix.  */
-        let right = parse_binary_op_with_precedence(&Expr::Void, tokens, precedence + 1)?;
-        result = Expr::BinaryOp {
-            left: Box::new(left.clone()),
+
+        let rhs = parse_single_expr(tokens)?;
+
+        let right = parse_binary_op_with_precedence(rhs, tokens, precedence + 1)?;
+
+        left = Expr::BinaryOp {
+            left: Box::new(left),
             right: Box::new(right),
             op,
             return_type: Type::Any,
         };
     }
 
-    Ok(result)
+    Ok(left)
 }
-
 fn operator_precedence(op: &Operation) -> u8 {
     match op {
         Operation::Multiply | Operation::Divide | Operation::Modulo => 3,
